e8021fc1145e9778acb9d2e125e2cec4d05ab762
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-12-14 20:38:37 +0000

    Get response fields and css selector from a config file

--- src/scraper.js
+++ src/scraper.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
+const config = require('./scraper.config.json');
 
 exports.getProduct = async function (url) {
+	let product = { url };
 	const userAgent =
 		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 	const browserLaunchOptions = {
@@ -14,23 +16,13 @@ exports.getProduct = async function (url) {
 	await page.setUserAgent(userAgent);
 	await page.goto(url, { waitUntil: 'networkidle2' });
 
-	const nameElt = await page.$('.indexes__StyledBaseTypography-wgki8j-99');
-	const name = await (await nameElt.getProperty('innerText')).jsonValue();
-
-	const priceElt = await page.$('.eIewAh');
-	const price = await (await priceElt.getProperty('innerText')).jsonValue();
-
-	const typeElt = await page.$(
-		'div.indexes__StyledAttributeItem-wgki8j-0:nth-child(3) > div:nth-child(2) > p:nth-child(2)'
-	);
-	const type = await (await typeElt.getProperty('innerText')).jsonValue();
-
-	const regionElt = await page.$(
-		'div.indexes__StyledAttributeItem-wgki8j-0:nth-child(4) > div:nth-child(2) > p:nth-child(2)'
-	);
-	const region = await (await regionElt.getProperty('innerText')).jsonValue();
+	for (const field of config.fields) {
+		const element = await page.$(field.selector);
+		const innerText = await (await element.getProperty('innerText')).jsonValue();
+		product[field.field] = innerText;
+	}
 
 	await browser.close();
 
-	return { url, name, type, price, region };
+	return product;
 };
