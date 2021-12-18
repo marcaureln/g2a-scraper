1c91269d3d50cd4e7368a04c1efa8cdc4cd2c6ae
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-12-18 10:53:38 +0000

    Connect to a separate browserless chrome instance

--- src/scraper.js
+++ src/scraper.js
@@ -5,12 +5,7 @@ exports.getProduct = async function (url) {
 	let product = { url };
 	const userAgent =
 		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
-	const browserLaunchOptions = {
-		headless: true,
-		args: ['--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox'],
-	};
-
-	const browser = await puppeteer.launch(browserLaunchOptions);
+	const browser = await puppeteer.connect({ browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT });
 
 	const page = await browser.newPage();
 	await page.setUserAgent(userAgent);
@@ -22,7 +17,7 @@ exports.getProduct = async function (url) {
 		product[field.field] = innerText;
 	}
 
-	await browser.close();
+	browser.disconnect();
 
 	return product;
 };
