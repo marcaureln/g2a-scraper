e517483c066347d42c70e3e24251ad0b7da76f94
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-12-22 17:08:14 +0000

    Use randomly generated user agents

--- src/scraper.js
+++ src/scraper.js
@@ -1,14 +1,14 @@
+const UserAgent = require('user-agents');
 const puppeteer = require('puppeteer');
 const config = require('./scraper.config.json');
 
 exports.getProduct = async function (url) {
 	let product = { url };
-	const userAgent =
-		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
+	const userAgent = new UserAgent({ deviceCategory: 'desktop' });
 	const browser = await puppeteer.connect({ browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT });
 
 	const page = await browser.newPage();
-	await page.setUserAgent(userAgent);
+	await page.setUserAgent(userAgent.toString());
 	await page.goto(url, { waitUntil: 'domcontentloaded' });
 
 	for (const field of config.fields) {
